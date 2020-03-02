c608eed129c8724c774f139461f8df8b963ba33b
slikts <dabas@untu.ms>
2020-03-02 16:52:55 +0000

    gravity stub

---
+++ src/util/useTiltGravity.ts
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import { Common } from 'matter-js';
+import { useEngine } from '../Engine';
+
+const useTiltGravity = () => {
+  const engine = useEngine();
+
+  useEffect(() => {
+    const updateGravity = (event: DeviceOrientationEvent) => {
+      // TODO: doesn't work
+      const orientation = window.orientation || 0;
+      const { gravity } = engine.world;
+
+      if (orientation === 0) {
+        gravity.x = Common.clamp(event.gamma, -90, 90) / 90;
+        gravity.y = Common.clamp(event.beta, -90, 90) / 90;
+      } else if (orientation === 180) {
+        gravity.x = Common.clamp(event.gamma, -90, 90) / 90;
+        gravity.y = Common.clamp(-event.beta!, -90, 90) / 90;
+      } else if (orientation === 90) {
+        gravity.x = Common.clamp(event.beta, -90, 90) / 90;
+        gravity.y = Common.clamp(-event.gamma!, -90, 90) / 90;
+      } else if (orientation === -90) {
+        gravity.x = Common.clamp(-event.beta!, -90, 90) / 90;
+        gravity.y = Common.clamp(event.gamma, -90, 90) / 90;
+      }
+      window.location.hash = `${gravity.x}-${gravity.y}`;
+    };
+
+    window.addEventListener('deviceorientation', updateGravity);
+
+    return () =>
+      void window.removeEventListener('deviceorientation', updateGravity);
+  }, [engine]);
+};
+
+export default useTiltGravity;
+
+declare module 'matter-js' {
+  export const Common: any;
+}
