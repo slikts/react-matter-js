238c53203d33e16e69ffe5588f9643dc0ea0108d
slikts <dabas@untu.ms>
2020-03-04 19:33:40 +0000

    export

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 export { default as Engine } from './Engine';
 export { default as Render } from './Render';
-export { default as RenderClones } from './extra/RenderClones';
 export { default as Body } from './bodies/Body';
 export { default as Rectangle } from './bodies/Rectangle';
 export { default as Circle } from './bodies/Circle';
 export { default as Vertices } from './bodies/Vertices';
 export { default as Constraint } from './Constraint';
 // Extras
+export { default as RenderClones } from './extra/RenderClones';
 export { default as Scene } from './extra/Scene';
 export { default as WindowScene } from './extra/WindowScene';
 export { default as Shape } from './extra/Shape';
